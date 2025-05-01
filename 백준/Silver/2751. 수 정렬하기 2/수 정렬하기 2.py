3333672d0e1058eceef1e534a163fa4e03188e16
L-and <53785896+L-and@users.noreply.github.com>
2025-05-01 11:19:41 +0900

    [Silver V] Title: 수 정렬하기 2, Time: 1856 ms, Memory: 222432 KB -BaekjoonHub

---
+++ 백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.py
@@ -0,0 +1,12 @@
+num_list_len = int(input())
+num_list = list()
+
+for i in range(num_list_len):
+    num_list.append(int(input()))
+
+num_list = sorted(num_list)
+
+for i in range(num_list_len):
+    print(num_list[i])
+
+                   
