8afae880313a58504930b7759b3d928b9e1b14f3
L-and <53785896+L-and@users.noreply.github.com>
2025-05-07 08:39:14 +0900

    [Silver II] Title: A → B, Time: 20 ms, Memory: 6648 KB -BaekjoonHub

---
+++ 백준/Silver/16953. A → B/A → B.cc
@@ -0,0 +1,55 @@
+#include <iostream>
+#include <queue>
+#include <algorithm>
+#include <map>
+using namespace std;
+
+int main()
+{
+    long long startNum, targetNum;
+
+    cin >> startNum >> targetNum;
+
+    int result = -1;
+    bool findFlag = false;
+
+    queue<pair<long long, int>> q;
+    map<long long, bool> visit;
+
+    q.push(make_pair(startNum, 1));
+    visit[startNum] = true;
+
+    while (q.size() != 0)
+    {
+        auto curPair = q.front(); q.pop();
+        long long curNum = curPair.first;
+        int curCnt = curPair.second;
+
+        // cout << curNum << ", " << curCnt << endl;
+
+        if (curNum == targetNum)
+        {
+            result = curCnt;
+            break;
+        }
+
+        // 새로운 숫자를 큐에 추가
+        long long num1 = (curNum * 2);
+        long long num2 = (curNum * 10 + 1);
+        if (num1 <= targetNum && !visit[num1])
+        {
+            q.push(make_pair(num1, curCnt+1));
+            visit[num1] = true;
+        }
+
+        if (num2 <= targetNum && !visit[num2])
+        {
+            q.push(make_pair(num2, curCnt+1));
+            visit[num2] = true;
+        }
+    }
+
+    cout << result;
+
+    return 0;
+}
